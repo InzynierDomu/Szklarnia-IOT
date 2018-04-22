5344cb80970fd1600f6d3b49189b63fb5e3ca7ee
DESKTOP-NP79D21\Omen <szymon.mmmmm@gmail.com>
2018-04-22 13:49:13 +0200

    Create plot view model

---
+++ PC_App/PC_App/Models/DBconnect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_App.Models
+{
+    class DBconnect
+    {
+    }
+}
---
+++ PC_App/PC_App/ViewModels/Plot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PC_App.Models;
+using OxyPlot;
+
+namespace PC_App.ViewModels
+{
+    class Plot
+    {
+        public IList<DataPoint> TempLvlPoints { get; private set; }
+        public IList<DataPoint> HumLvlPoints { get; private set; }
+        public IList<DataPoint> LightLvlPoints { get; private set; }
+        public IList<DataPoint> WaterLvlPoints { get; private set; }
+        public IList<DataPoint> LightPoints { get; private set; }
+
+
+        public Plot(DBconnect Database)
+        {
+            TempLvlPoints = new List<DataPoint>();
+            //ConvertPointsTemp(Database);
+        }
+
+        //public void ConvertPointsTemp(DBconnect database)
+        //{
+        //    //Points.Clear();
+        //    foreach (temp point in database.Database)
+        //    {
+        //        DataPoint tempPoint = new DataPoint(point.Id, point.temperature);
+        //        Points.Add(tempPoint);
+        //    }
+        //}
+    }
+}
